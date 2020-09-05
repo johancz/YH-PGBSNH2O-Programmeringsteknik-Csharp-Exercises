528219ba5e5da9a1446031ed7f78b2766b790c1f
JoCh <johancz@users.noreply.github.com>
2020-09-06 00:33:36 +0200

    Added lektion-4-exercise-6 variant2 and fixed earlier tests.

--- Lektion-4-Exercise-6/Program.cs
+++ Lektion-4-Exercise-6/Program.cs
@@ -24,14 +24,39 @@ namespace Lektion_4_Exercise_6
             // We need this to make sure we can always use periods for decimal points.
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
-            Run_variant0();
-            Console.WriteLine(Environment.NewLine);
-            Run_variant1();
+            //if (int.TryParse(Console.ReadLine(), out int input))
+            //while (true)
+            {
+                Console.WriteLine("Choose variant (0-3), or press 'x' to exit.");
+                string input = Console.ReadLine();
+
+                switch (input) {
+                    case "0":
+                        Run_variant0();
+                        break;
+                    case "1":
+                        Run_variant1();
+                        break;
+                    case "2":
+                        Run_variant2();
+                        break;
+                    case "3":
+                        break;
+                    case "x":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("That is not a variant, goodbye.");
+                        break;
+                }
+
+                Console.WriteLine(Environment.NewLine);
+            }
         }
 
         public static void Run_variant0()
         {
-            Console.WriteLine("Variant 0:");
+            Console.WriteLine("::: Variant 0 :::");
             //int dice1_sides = 6;
             //int dice2_sides = 6;
             //var d1 = new { min = (int)1, max = (int)6 };
@@ -55,7 +80,7 @@ namespace Lektion_4_Exercise_6
         /* Write a variant of the program that reads an integer from the user and only shows those pairs whose sum is that high or higher.For example, if 10 is entered, the program should only print the following pairs: (4, 6), (5, 5), (5, 6), (6, 4), (6, 5), (6, 6). */
         public static void Run_variant1()
         {
-            Console.WriteLine("Variant 1:");
+            Console.WriteLine("::: Variant 1 :::");
 
             Console.Write("Enter a number: ");
 
@@ -79,6 +104,34 @@ namespace Lektion_4_Exercise_6
                 Console.Write("Output: " + string.Join(", ", output));
             }
         }
+
+        /* Write a variant of the first program that can be used by a tabletop roleplayer with a flaming longsword. In other words, one of the dice should have eight sides (and the other still have six sides).
+         * 
+         * Question: Can you imagine two different but very similar ways of achieving this?
+         * Answer: While-loops?
+         */
+        public static void Run_variant2()
+        {
+            Console.WriteLine("::: Variant 2 :::");
+
+            Dice flamingLongsword = new Dice(1, 8);
+            Dice normalDice = new Dice(1, 6);
+            List<string> output = new List<string>();
+
+            for (int i1 = flamingLongsword.min; i1 <= flamingLongsword.max; ++i1)
+            {
+                int i2 = normalDice.min;
+
+                while (i2 <= normalDice.max)
+                {
+                    output.Add($"({i1}, {i2})");
+
+                    i2++;
+                }
+            }
+
+            Console.Write("Output: " + string.Join(", ", output));
+        }
     }
 
     [TestClass]
@@ -87,15 +140,15 @@ namespace Lektion_4_Exercise_6
         [TestMethod]
         public void Test_variant0()
         {
-            using FakeConsole console = new FakeConsole("");
+            using FakeConsole console = new FakeConsole("0", "x");
             Program.Main();
-            Assert.AreEqual("Output: (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6)", console.Lines[1]);
+            Assert.AreEqual("Output: (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6)", console.Output);
         }
 
         [TestMethod]
         public void Test_variant1_test1()
         {
-            using FakeConsole console = new FakeConsole("10");
+            using FakeConsole console = new FakeConsole("1", "10", "x");
             Program.Main();
             Assert.AreEqual("Output: (4, 6), (5, 5), (5, 6), (6, 4), (6, 5), (6, 6)", console.Output);
         }
@@ -103,9 +156,17 @@ namespace Lektion_4_Exercise_6
         [TestMethod]
         public void Test_variant1_test2()
         {
-            using FakeConsole console = new FakeConsole("13");
+            using FakeConsole console = new FakeConsole("1", "13", "x");
             Program.Main();
             Assert.AreEqual("Output:", console.Output);
+        }
+
+        [TestMethod]
+        public void Test_variant2_test1()
+        {
+            using FakeConsole console = new FakeConsole("2", "x");
+            Program.Main();
+            Assert.AreEqual("Output: (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6)", console.Output);
         }
     }
 }
