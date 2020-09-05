1a8123f21a385753cf37c4bdebb2e4a368dbe862
JoCh <johancz@users.noreply.github.com>
2020-09-05 19:13:06 +0200

    Fixed variant names and added the actual "variant1. Updated variant0's test.
    * Variant1 is now variant0, and variant2 is variant 1.
    * Updated variant0's test to work with multiple lines.

--- Lektion-4-Exercise-6/Program.cs
+++ Lektion-4-Exercise-6/Program.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -24,11 +26,14 @@ namespace Lektion_4_Exercise_6
             // We need this to make sure we can always use periods for decimal points.
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
+            Run_variant0();
+            Console.WriteLine(Environment.NewLine);
             Run_variant1();
         }
 
-        public static void Run_variant1()
+        public static void Run_variant0()
         {
+            Console.WriteLine("Variant 0:");
             //int dice1_sides = 6;
             //int dice2_sides = 6;
             //var d1 = new { min = (int)1, max = (int)6 };
@@ -38,6 +43,7 @@ namespace Lektion_4_Exercise_6
 
             Dice d1 = new Dice(1, 6);
             Dice d2 = new Dice(1, 6);
+            Console.Write("Output: ");
 
             for (int i1 = d1.min; i1 <= d1.max; ++i1)
             {
@@ -47,17 +53,61 @@ namespace Lektion_4_Exercise_6
                 }
             }
         }
+
+        /* Write a variant of the program that reads an integer from the user and only shows those pairs whose sum is that high or higher.For example, if 10 is entered, the program should only print the following pairs: (4, 6), (5, 5), (5, 6), (6, 4), (6, 5), (6, 6). */
+        public static void Run_variant1()
+        {
+            Console.WriteLine("Variant 1:");
+
+            Console.Write("Enter a number: ");
+
+            if (int.TryParse(Console.ReadLine(), out int input))
+            {
+                Dice d1 = new Dice(1, 6);
+                Dice d2 = new Dice(1, 6);
+                List<string> output = new List<string>();
+
+                for (int i1 = d1.min; i1 <= d1.max; ++i1)
+                {
+                    for (int i2 = d2.min; i2 <= d2.max; ++i2)
+                    {
+                        if (i1 + i2 >= input)
+                        {
+                            output.Add($"({i1}, {i2})");
+                        }
+                    }
+                }
+
+                Console.Write("Output: " + string.Join(", ", output));
+            }
+        }
     }
 
     [TestClass]
     public class ProgramTests
     {
         [TestMethod]
-        public void ExampleTest()
+        public void Test_variant0()
         {
-            using FakeConsole console = new FakeConsole();
+            using FakeConsole console = new FakeConsole("");
             Program.Main();
-            Assert.AreEqual("(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6)", console.Output);
+            Assert.AreEqual("Output: (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6)", console.Lines[1]);
+        }
+
+        [TestMethod]
+        public void Test_variant1_test1()
+        {
+            using FakeConsole console = new FakeConsole("10");
+            Program.Main();
+            Assert.AreEqual("Output: (4, 6), (5, 5), (5, 6), (6, 4), (6, 5), (6, 6)", console.Output);
+        }
+
+        [TestMethod]
+        public void Test_variant1_test2()
+        {
+            using FakeConsole console = new FakeConsole("13");
+            Program.Main();
+            Assert.AreEqual("Output:", console.Output);
         }
     }
 }
