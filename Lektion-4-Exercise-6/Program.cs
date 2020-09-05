e011365b026d414ce151e26ee288c5a4ccacbd2f
JoCh <johancz@users.noreply.github.com>
2020-09-05 19:16:44 +0200

    Removed unused includes.

--- Lektion-4-Exercise-6/Program.cs
+++ Lektion-4-Exercise-6/Program.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Lektion_4_Exercise_6
