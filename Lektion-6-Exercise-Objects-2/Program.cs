4461e72ef0f7c5dfef30c63185f24a38316e976a
JoCh <johancz@users.noreply.github.com>
2020-09-12 23:39:44 +0200

    Added Lektion-6-Exercise-Objects-2 with 1 test.

---
+++ Lektion-6-Exercise-Objects-2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lektion_6_Exercise_Objects_2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // We need this to make sure we can always use periods for decimal points.
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.Write("Enter a personnummer: ");
+            string personnummer = Console.ReadLine();
+            personnummer = personnummer.Length > 0 ? personnummer : "19810203-1234";
+
+            string yearOfBirth = personnummer.Substring(0, 4);
+            //string gender = (personnummer.Substring(personnummer.Length - 2, 1) % 2 == 0) ? "woman" : "man";
+            string gender = (personnummer[11] % 2 == 0) ? "woman" : "man";
+
+            Console.WriteLine("This is a " + gender + " born in " + yearOfBirth);
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            using FakeConsole console = new FakeConsole("19810203-1234");
+            Program.Main();
+            Assert.AreEqual("This is a man born in 1981", console.Output);
+        }
+    }
+}
