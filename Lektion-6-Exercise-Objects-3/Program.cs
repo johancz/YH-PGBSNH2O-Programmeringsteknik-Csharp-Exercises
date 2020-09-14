a7ce4925c0d1725a087fa3a45e4fd45b19a6e075
JoCh <johancz@users.noreply.github.com>
2020-09-14 19:46:47 +0200

    Added Lektion-6-Exercise-Objects-3

---
+++ Lektion-6-Exercise-Objects-3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lektion_6_Exercise_Objects_3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // We need this to make sure we can always use periods for decimal points.
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Enter text to automatically redact sensitive information from it: ");
+            string input = Console.ReadLine();
+            if (input.Length == 0) { input = "Classified: There are extraterrestrials at Area 51. Don't trespass into Area 51."; }
+            
+            string inputRedacted = input;
+            if (input.StartsWith("Classified:")) {
+                inputRedacted = inputRedacted.Remove(0, 11);
+                inputRedacted = inputRedacted.Replace("Area 51", "[AN UNDISCLOSED LOCATION]");
+                inputRedacted = inputRedacted.Trim();
+            }
+
+            Console.WriteLine(inputRedacted);
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            using FakeConsole console = new FakeConsole("Classified: There are extraterrestrials at Area 51. Don't trespass into Area 51.");
+            Program.Main();
+            Assert.AreEqual("There are extraterrestrials at [AN UNDISCLOSED LOCATION]. Don't trespass into [AN UNDISCLOSED LOCATION].", console.Output);
+        }
+    }
+}
