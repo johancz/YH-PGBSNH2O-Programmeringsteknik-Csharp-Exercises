ffbb198bc813dce7d99ff547ae4818e130c962da
JoCh <johancz@users.noreply.github.com>
2020-09-18 22:32:24 +0200

    Added exercise and incomplete wip on a variant.
    Added:
    * Session-7-Exercise-problem-solving-8-censor-string with 1 test.
    * (incomplete wip) Session-7-Exercise-problem-solving-8-censor-string-variant-1

---
+++ Session-7-Exercise-problem-solving-8-censor-string-variant-1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Session_7_Exercise_problem_solving_8_censor_string_variant_1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // We need this to make sure we can always use periods for decimal points.
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Enter string to censor it:");
+            string input_string = Console.ReadLine();
+            //if (input_string.Length == 0) { input_string = "Luke's father is Vader and Rosebud is a sled"; }
+            if (input_string.Length == 0) { input_string = "Vader Vader sledo"; }
+            Console.WriteLine("Enter a list of the words you wish to censor (seperate with a space):");
+            string input_wordsToCensor = Console.ReadLine();
+            if (input_wordsToCensor.Length == 0) { input_wordsToCensor = "Vader sled"; }
+
+            //List<string> words = input_string.Split(new[] { ' ', '.', ',', ';', ':', '"', '\'', '!', '?', '_' }).ToList();
+            List<string> words = new List<string>();
+            List<string> separators = new List<string>();
+            string word = "";
+            int words_i = 0;
+            for (int i = 0, last_i = input_string.Length - 1; i < input_string.Length; i++)
+            {
+                char c = input_string[i];
+                bool isLetterOrDigit = char.IsLetterOrDigit(c);
+
+                if (i == last_i)
+                {
+                    word += c;
+                    words.Add(word);
+                    word = "";
+                }
+                else if ()
+
+
+                    //////////
+                    if (char.IsLetterOrDigit(c))
+                    {
+                        word += c;
+                    }
+
+                if (!char.IsLetterOrDigit(c) || i == last_i)
+                {
+                    words.Add(word);
+                    //words.Add(c.ToString());
+                }
+
+                /////////////////
+
+                if (char.IsLetterOrDigit(c))
+                {
+                    word += c;
+
+                    if (i == last_i)
+                    {
+                        words.Add(word);
+                    }
+                }
+                else
+                {
+                    words.Add(word);
+                    words.Add(c.ToString());
+                    word = "";
+                }
+            }
+
+            string[] wordsToCensor = input_wordsToCensor.Split(' ');
+            string input_string_censored = input_string;
+
+            Console.WriteLine(string.Join(" ", input_string_censored));
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            using FakeConsole console = new FakeConsole("First input", "Second input");
+            Program.Main();
+            Assert.AreEqual("Hello!", console.Output);
+        }
+    }
+}
---
+++ Session-7-Exercise-problem-solving-8-censor-string/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Session_7_Exercise_problem_solving_8_censor_string
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // We need this to make sure we can always use periods for decimal points.
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Enter string to censor it:");
+            string input_string = Console.ReadLine();
+            //if (input_string.Length == 0) { input_string = "Luke's father is Vader and Rosebud is a sled"; }
+            if (input_string.Length == 0) { input_string = "Vader Vadero sled sledo"; }
+            Console.WriteLine("Enter a list of the words you wish to censor (separate with a space):");
+            string input_wordsToCensor = Console.ReadLine();
+            if (input_wordsToCensor.Length == 0) { input_wordsToCensor = "Vader sled"; }
+            List<string> words = input_string.Split(' ').ToList();
+            string[] wordsToCensor = input_wordsToCensor.Split(' ');
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                string word = words[i];
+
+                foreach (string wordToCensor in wordsToCensor)
+                {
+                    if (word == wordToCensor)
+                    {
+                        // Can't use 'word' here since it's a value and not a reference.
+                        words[i] = new string('*', word.Length);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", words));
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            using FakeConsole console = new FakeConsole("Vader Vadero sled sledo", "Vader sled");
+            Program.Main();
+            Assert.AreEqual("***** Vadero **** sledo", console.Output);
+        }
+    }
+}
