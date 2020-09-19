2c08ca60e14c323703dc7563f5a44f070619b2fa
JoCh <johancz@users.noreply.github.com>
2020-09-20 01:59:07 +0200

    Added exercises
    Session-7-Exercise-problem-solving-...:
    * ...9-translate with dictionary with 2 tests.
    * ...10-compress string with 1 test.
    * ...11-dice (bad code) with 12 tests.

---
+++ Session-7-Exercise-problem-solving-10-compress-string/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Session_7_Exercise_problem_solving_10_compress_string
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // We need this to make sure we can always use periods for decimal points.
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Enter the text you want to compress:");
+            string input = Console.ReadLine().Trim();
+            if (input.Length == 0) { input = "aaaaaabbbbbccccccccccccddeeeeeeeeeeeeeefffg".Trim(); }
+
+            string compressedString = Compress(input);
+            Console.WriteLine("Original:\t" + input);
+            Console.WriteLine("Compressed:\t" + compressedString);
+            Console.WriteLine("Decompressed:\t" + Decompress(compressedString));
+        }
+        
+        public static string Compress(string s)
+        {
+            string s2 = "" + s[0];
+            int charCounter = 1;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s2.Last())
+                {
+                    charCounter++;
+                }
+                else
+                {
+                    s2 += charCounter;
+                    s2 += s[i];
+                    charCounter = 1;
+                }
+            }
+            s2 += charCounter;
+
+            return s2;
+        }
+
+        public static string Decompress(string s)
+        {
+            string s2 = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+
+                if (char.IsLetter(c))
+                {
+                    string repeatCount = "";
+
+                    while (++i < s.Length && char.IsDigit(s[i]))
+                    {
+                        repeatCount += s[i];
+                    }
+
+                    s2 += new string(c, int.Parse(repeatCount));
+
+                    i--;
+                }
+            }
+
+            return s2;
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            using FakeConsole console = new FakeConsole("aaaaaabbbbbccccccccccccddeeeeeeeeeeeeeefffg");
+            Program.Main();
+            CollectionAssert.AreEqual(new[] {
+                "Original:\taaaaaabbbbbccccccccccccddeeeeeeeeeeeeeefffg",
+                "Compressed:\ta6b5c12d2e14f3g1",
+                "Decompressed:\taaaaaabbbbbccccccccccccddeeeeeeeeeeeeeefffg"
+            }, console.Lines);
+        }
+    }
+}
---
+++ Session-7-Exercise-problem-solving-11-dice/Program.cs
@@ -0,0 +1,240 @@
+﻿/* Session_7_Exercise_problem_solving_11_dice:
+ * 
+ * Write a program that reads a string from the user describing a dice roll
+ * and then performs that dice roll (using the Random class) and prints the result.
+ * 
+ * The rules of the string are as follows: the number of dice is specified first,
+ * followed by the character 'd' (for “dice”), followed by the number of sides on
+ * each die (which doesn't need to be 6), optionally followed by '+' or '-' and a
+ * number that should be added or subtracted from the roll.
+ */
+
+using System;
+using System.Globalization;
+#if DEBUG
+using System.Text.RegularExpressions; // We only need this for the unit tests.
+using Microsoft.VisualStudio.TestTools.UnitTesting; // We only need this for the unit tests.
+#endif
+
+namespace Session_7_Exercise_problem_solving_11_dice
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // We need this to make sure we can always use periods for decimal points.
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.Write("Enter a dice string to parse:");
+            string input = Console.ReadLine();
+            if (input.Length == 0) { input = "3d10+2"; }
+
+            ParseDiceRoll(input);
+        }
+
+        public static void ParseDiceRoll(string diceRoll)
+        {
+            Console.WriteLine("Dice roll string:\t " + diceRoll);
+            //string[] input_array = input.Split(new[] { 'd', '+', '-' });
+            int i_of_d = diceRoll.IndexOf('d');
+
+            // Get string after 'd'.
+            if (i_of_d > 0 && i_of_d + 1 < diceRoll.Length)
+            {
+                int current_i = i_of_d + 1;
+
+                // NumberOfDice
+                string numberOfDice = diceRoll.Substring(0, i_of_d);
+
+                // Check for '-' or '+'.
+                int i_of_plus = diceRoll.IndexOf('+', current_i);
+                int i_of_minus = diceRoll.IndexOf("-", current_i);
+                int i_of_modifier = -1;
+                int len_of_sidesString = 0;
+                if (i_of_plus > current_i || i_of_minus > current_i)
+                {
+                    i_of_modifier = (i_of_minus > i_of_plus ? i_of_minus : i_of_plus);
+                    len_of_sidesString = i_of_modifier  - current_i;
+                }
+                else
+                {
+                    len_of_sidesString = diceRoll.Length - current_i;
+                }
+
+                if (len_of_sidesString > 0)
+                {
+
+                    // Number of sides on each die.
+                    string diceSides = diceRoll.Substring(current_i, len_of_sidesString);
+
+
+                    //if (i_of_d < i_of_plus && i_ + 1 < diceRoll.Length) // && i_of_minus + 1 < input.Length)
+                    //{
+                    //}
+
+                    if (int.TryParse(numberOfDice, out int dieCount) && int.TryParse(diceSides, out int sideCount))
+                    {
+                        Random random = new Random();
+                        int sumOfRolls = 0;
+                        for (int i = 0; i < dieCount; i++)
+                        {
+                            // 1 sided die? a möbius strip?
+                            sumOfRolls += random.Next(1, sideCount);
+                            //Console.WriteLine("sumOfRolls: " + sumOfRolls);
+                        }
+
+                        //int modifierValue = 0;
+                        if (i_of_modifier > -1
+                            && (i_of_modifier + 1) < diceRoll.Length
+                            && int.TryParse(diceRoll.Substring(i_of_modifier), out int modifierValue))
+                        {
+                            //char modifierType = diceRoll[i_of_modifier];
+
+                            //if (modifierType == '-')
+                            //{
+                                sumOfRolls += modifierValue;
+                            //}
+                            //else if (modifierType == '+')
+                            //{
+                            //}
+
+                        }
+
+
+                        //Console.WriteLine("i_of_plus:\t " + i_of_plus);
+                        //Console.WriteLine("i_of_minus:\t " + i_of_minus);
+                        //Console.WriteLine("i_of_modifier:\t " + len_of_sidesString);
+                        //Console.WriteLine("current_i:\t " + current_i);
+                        //Console.WriteLine("# of dice:\t " + numberOfDice);
+                        //Console.WriteLine("# of die sides:\t " + diceSides);
+                        Console.WriteLine("Sum of rolls:\t " + sumOfRolls);
+                        Console.WriteLine();
+
+                        // The string was succesfully parsed, exit out of method.
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("Invalid dice roll string format!");
+            Console.WriteLine();
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            using FakeConsole console = new FakeConsole("3d10+");
+            Program.Main();
+            Assert.AreEqual("Dice roll string:\t 3d10+", console.Lines[0]);
+            //StringAssert.StartsWith(console.Output, "Sum of rolls:\t ");
+            StringAssert.Matches(console.Output, new Regex(@"Sum of rolls:\t \d+"));
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            using FakeConsole console = new FakeConsole("3d5+");
+            Program.Main();
+            Assert.AreEqual("Dice roll string:\t 3d5+", console.Lines[0]);
+            //StringAssert.StartsWith(console.Output, "Sum of rolls:\t ");
+            StringAssert.Matches(console.Output, new Regex(@"Sum of rolls:\t \d+"));
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            using FakeConsole console = new FakeConsole("3d10");
+            Program.Main();
+            Assert.AreEqual("Dice roll string:\t 3d10", console.Lines[0]);
+            //StringAssert.StartsWith(console.Output, "Sum of rolls:\t ");
+            StringAssert.Matches(console.Output, new Regex(@"Sum of rolls:\t \d+"));
+        }
+        [TestMethod]
+        public void Test4()
+        {
+            using FakeConsole console = new FakeConsole("3d5");
+            Program.Main();
+            Assert.AreEqual("Dice roll string:\t 3d5", console.Lines[0]);
+            //StringAssert.StartsWith(console.Output, "Sum of rolls:\t ");
+            StringAssert.Matches(console.Output, new Regex(@"Sum of rolls:\t \d+"));
+        }
+        [TestMethod]
+        public void Test5()
+        {
+            using FakeConsole console = new FakeConsole("d1");
+            Program.Main();
+            CollectionAssert.AreEqual(new[] {
+                "Dice roll string:\t d1",
+                "Invalid dice roll string format!"
+            }, console.Lines);
+        }
+        [TestMethod]
+        public void Test6()
+        {
+            using FakeConsole console = new FakeConsole("d");
+            Program.Main();
+            CollectionAssert.AreEqual(new[] {
+                "Dice roll string:\t d",
+                "Invalid dice roll string format!"
+            }, console.Lines);
+        }
+        [TestMethod]
+        public void Test7()
+        {
+            using FakeConsole console = new FakeConsole("d10");
+            Program.Main();
+            CollectionAssert.AreEqual(new[] {
+                "Dice roll string:\t d10",
+                "Invalid dice roll string format!"
+            }, console.Lines);
+        }
+        [TestMethod]
+        public void Test8()
+        {
+            using FakeConsole console = new FakeConsole("5d6-5");
+            Program.Main();
+            Assert.AreEqual("Dice roll string:\t 5d6-5", console.Lines[0]);
+            StringAssert.Matches(console.Output, new Regex(@"Sum of rolls:\t \d+"));
+        }
+        [TestMethod]
+        public void Test9()
+        {
+            using FakeConsole console = new FakeConsole("5d6-1");
+            Program.Main();
+            Assert.AreEqual("Dice roll string:\t 5d6-1", console.Lines[0]);
+            StringAssert.Matches(console.Output, new Regex(@"Sum of rolls:\t \d+"));
+        }
+        [TestMethod]
+        public void Test10()
+        {
+            using FakeConsole console = new FakeConsole("5d6+-11");
+            Program.Main();
+            CollectionAssert.AreEqual(new[] {
+                "Dice roll string:\t 5d6+-11",
+                "Invalid dice roll string format!"
+            }, console.Lines);
+        }
+        [TestMethod]
+        public void Test11()
+        {
+            using FakeConsole console = new FakeConsole("5d6+-");
+            Program.Main();
+            CollectionAssert.AreEqual(new[] {
+                "Dice roll string:\t 5d6+-",
+                "Invalid dice roll string format!"
+            }, console.Lines);
+        }
+        [TestMethod]
+        public void Test12()
+        {
+            using FakeConsole console = new FakeConsole("5d6+10-11");
+            Program.Main();
+            CollectionAssert.AreEqual(new[] {
+                "Dice roll string:\t 5d6+10-11",
+                "Invalid dice roll string format!"
+            }, console.Lines);
+        }
+    }
+}
