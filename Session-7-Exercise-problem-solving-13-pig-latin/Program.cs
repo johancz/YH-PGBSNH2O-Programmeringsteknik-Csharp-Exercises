bcc771240f9c8c29d4e323c13e2c443ecab889ab
JoCh <johancz@users.noreply.github.com>
2020-09-20 21:45:15 +0200

    Updated 'Session-7-Exercise-problem-solving-13-pig-latin' with fixes and tests.

--- Session-7-Exercise-problem-solving-13-pig-latin/Program.cs
+++ Session-7-Exercise-problem-solving-13-pig-latin/Program.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Session_7_Exercise_problem_solving_13_pig_latin
@@ -23,13 +24,11 @@ namespace Session_7_Exercise_problem_solving_13_pig_latin
             Console.WriteLine("Input the text you want to translate to \"pig latin\":");
             char[] consonants = new char[] {
                 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r',
-                's', 't', 'v', 'w', 'x', 'y', 'z', 'B', 'C', 'D', 'F', 'G', 'H', 'J',
-                'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'
+                's', 't', 'v', 'w', 'x', 'z', 'B', 'C', 'D', 'F', 'G', 'H', 'J',
+                'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z'
             };
 
             string[] input = Console.ReadLine().Split(' ');
-            input = "Wikipedia".Split(' ');
-            //input = "How do you say ... in Pig Latin?".Split(' ');
             List<string> translatedText = new List<string>();
 
             foreach (string word in input)
@@ -40,9 +39,14 @@ namespace Session_7_Exercise_problem_solving_13_pig_latin
                 {
                     char c = word[i];
 
-                    if (!consonants.Contains(c))
-                    {
+                    Console.WriteLine((i == 0 && (c == 'y' || c == 'Y')) == (i > 0 && !(c == 'y' || c == 'Y')));
 
+                    // If 'y' || 'Y' is the first letter of the word, treat it as a consonant, otherwise a vowel.
+                    if (i == 0 && (c == 'y' || c == 'Y'))
+                    {
+                    }
+                    else if (!consonants.Contains(c))
+                    {
                         newWord += word.Substring(i);
                         break;
                     }
@@ -56,7 +60,20 @@ namespace Session_7_Exercise_problem_solving_13_pig_latin
                 }
                 else
                 {
-                    newWord += initialConsonants + "ay";
+                    // Decapitalize the first consonant that was moved.
+                    bool firstWasUpper = char.IsUpper(initialConsonants[0]);
+                    newWord += char.ToLower(initialConsonants[0]);
+                    if (initialConsonants.Length > 1)
+                    {
+                        newWord += initialConsonants.Substring(1);
+                    }
+
+                    newWord += "ay";
+
+                    // Capitilize the first letter of the word if the previous was.
+                    if (firstWasUpper) {
+                        newWord = char.ToUpper(newWord[0]) + newWord.Substring(1);
+                    }
                 }
 
                 translatedText.Add(newWord);
@@ -71,11 +88,67 @@ namespace Session_7_Exercise_problem_solving_13_pig_latin
     public class ProgramTests
     {
         [TestMethod]
-        public void ExampleTest()
+        public void Test1()
         {
-            using FakeConsole console = new FakeConsole("First input", "Second input");
+            using FakeConsole console = new FakeConsole("Wikipedia");
             Program.Main();
-            Assert.AreEqual("Hello!", console.Output);
+            Assert.AreEqual("Ikipediaway", console.Output);
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            using FakeConsole console = new FakeConsole("How do you say in Pig Latin");
+            Program.Main();
+            Assert.AreEqual("Owhay oday ouyay aysay inyay Igpay Atinlay", console.Output);
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            using FakeConsole console = new FakeConsole("HLT");
+            Program.Main();
+            Assert.AreEqual("HLTay", console.Output);
+        }
+        [TestMethod]
+        public void Test4()
+        {
+            using FakeConsole console = new FakeConsole("AEY");
+            Program.Main();
+            Assert.AreEqual("AEYyay", console.Output);
+        }
+        [TestMethod]
+        public void Test5()
+        {
+            using FakeConsole console = new FakeConsole("strength");
+            Program.Main();
+            Assert.AreEqual("engthstray", console.Output);
+        }
+        [TestMethod]
+        public void Test6()
+        {
+            using FakeConsole console = new FakeConsole("eight");
+            Program.Main();
+            Assert.AreEqual("eightyay", console.Output);
+        }
+        [TestMethod]
+        public void Test7()
+        {
+            using FakeConsole console = new FakeConsole("Yearly");
+            Program.Main();
+            Assert.AreEqual("Earlyyay", console.Output);
+        }
+        [TestMethod]
+        public void Test8()
+        {
+            using FakeConsole console = new FakeConsole("Aye");
+            Program.Main();
+            Assert.AreEqual("Ayeyay", console.Output);
+        }
+        [TestMethod]
+        public void Test9()
+        {
+            using FakeConsole console = new FakeConsole("HOw do");
+            Program.Main();
+            Assert.AreEqual("Owhay oday", console.Output);
         }
     }
 }
