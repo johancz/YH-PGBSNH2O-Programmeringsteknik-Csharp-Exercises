aa311a97eeed714c56b72843bc3a80502f583cac
JoCh <johancz@users.noreply.github.com>
2020-10-14 22:11:10 +0200

    Added projects:"Session-13-Exercise-ObjectsToTuples" and "Session-13-Exercise-TuplesToObjects"
    * This is the started code for the two exercises, solutions are required and will be added in future commits.

---
+++ Session-13/Github/Session-13-Exercise-ObjectsToTuples/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Session_13_Exercise_ObjectsToTuples
+{
+    public class Point
+    {
+        public double X;
+        public double Y;
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Hello!");
+        }
+
+        public static double DistanceBetween(Point p1, Point p2)
+        {
+            double xDistance = p2.X - p1.X;
+            double yDistance = p2.Y - p1.Y;
+            double distance = Math.Sqrt(Math.Pow(xDistance, 2) + Math.Pow(yDistance, 2));
+            return distance;
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            Point p1 = new Point
+            {
+                X = 0,
+                Y = 0
+            };
+            Point p2 = new Point
+            {
+                X = 3,
+                Y = 4
+            };
+            double distance = Program.DistanceBetween(p1, p2);
+            Assert.AreEqual(5, distance, 0.1);
+        }
+    }
+}
---
+++ Session-13/Github/Session-13-Exercise-TuplesToObjects/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Session_13_Exercise_TuplesToObjects
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Hello!");
+        }
+
+        public static string SummarizePerson((string, int, double) person)
+        {
+            return person.Item1 + " is " + person.Item2 + " years old and " + person.Item3 + " meters tall";
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            var person = ("Brad", 56, 1.82);
+            string summary = Program.SummarizePerson(person);
+            Assert.AreEqual("Brad is 56 years old and 1.82 meters tall", summary);
+        }
+    }
+}
