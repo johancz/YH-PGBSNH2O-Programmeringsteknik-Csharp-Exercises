6a4b80d243271ba89e0a2f9b389fbe4e6bd222aa
JoCh <johancz@users.noreply.github.com>
2020-10-14 18:51:15 +0200

    Added answer/solution to project:"Session-13-Exercise-Enums".

--- Session-13/Github/Session-13-Exercise-Enums/Program.cs
+++ Session-13/Github/Session-13-Exercise-Enums/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* Exercise: Enums
+Utgå från koden i EnumExample.
+    1. Skriv om koden så att den använder enum istället för string till AgeCategory.
+    2. Vad är fördelarna med enum i ett sånt här scenario?
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -6,25 +12,77 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Session_13_Exercise_Enums
 {
+    public enum AgeCategory {
+        None,
+        Senior,
+        Child,
+        Adult
+    }
+    // alt.
+    //public enum AgeCategory
+    //{
+    //    Senior = 1,
+    //    Child = 2,
+    //    Adult = 3
+    //}
+
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public AgeCategory AgeCategory;
+    }
+
     public class Program
     {
         public static void Main()
         {
-            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            Person p = new Person
+            {
+                FirstName = "Brad",
+                LastName = "Pitt",
+                AgeCategory = AgeCategory.Adult,
+            };
+            var d = typeof(decimal).IsPrimitive;
+            var f = typeof(float).IsPrimitive;
+            var i = typeof(int).IsPrimitive;
+            var s = typeof(string).IsPrimitive;
+            var c = typeof(char).IsPrimitive;
 
-            Console.WriteLine("Hello!");
+            // 2. Fördelarna med enum är att du inte kan skriva fel i if-satsens uttryck.
+            // "AgeCategory.Senior" finns men "AgeCategory.Senor" finns inte. Kompilatorn/VShade klagat på att
+            // det inte finns en definition för "Senor" och därför inte kompilerat programmet.
+            // Däremot så hade programmet kompilerats om uttrycket var "p.AgeCategory = "Senor",
+            // och gjort så utan att producera några varningar.
+            // Det kan på så sätt skapa buggar som kan vara svåra att identifiera.
+            if (p.AgeCategory == AgeCategory.Senior)
+            {
+                Console.WriteLine("You will receive a pension.");
+            }
+            else if (p.AgeCategory == AgeCategory.Child)
+            {
+                Console.WriteLine("You will receive child benefit.");
+            }
+            else if (p.AgeCategory == AgeCategory.Adult)
+            {
+                Console.WriteLine("You will receive no extra money. :(");
+            }
+            else //  else if (p.AgeCategory == AgeCategory.None)
+            {
+                Console.WriteLine("Invalid category.");
+            }
         }
     }
 
-    [TestClass]
-    public class ProgramTests
-    {
-        [TestMethod]
-        public void ExampleTest()
-        {
-            using FakeConsole console = new FakeConsole("First input", "Second input");
-            Program.Main();
-            Assert.AreEqual("Hello!", console.Output);
-        }
-    }
+    //[TestClass]
+    //public class ProgramTests
+    //{
+    //    [TestMethod]
+    //    public void ExampleTest()
+    //    {
+    //        using FakeConsole console = new FakeConsole("First input", "Second input");
+    //        Program.Main();
+    //        Assert.AreEqual("Hello!", console.Output);
+    //    }
+    //}
 }
