9733dd610dcc5e73a3bb2b04782fcb2c12b26ae6
JoCh <johancz@users.noreply.github.com>
2020-10-14 18:06:07 +0200

    Updated "Session-13-Exercise-Nullable-value-types" with the answer/solution.

--- Session-13/Github/Session-13-Exercise-Nullable-value-types/Program.cs
+++ Session-13/Github/Session-13-Exercise-Nullable-value-types/Program.cs
@@ -24,32 +24,51 @@ namespace Session_13_Exercise_Nullable_value_types
                 }
                 else
                 {
+                    // An exception is thrown if the string can't be parsed to a number.
                     int n = int.Parse(s);
                     numbers.Add(n);
                 }
             }
 
-            int largest = -1;
+            // Numbers can be negative.
+            // In the unmodified code (commented) below, if no numbers are entered by the user, -1 is printed as the largest number.
+            // This behaviour is incorrect as -1 was not entered by the user.
+            //int largest = -1;
+
+            // There are at least two ways to solve this;
+            // 1) by checking the length of the array of numbers entered by the user,
+            // 2) by making the variable "largest" nullable, "int? largest".
+            // Solution 2) is the most applicable for the purpose of this exercise.
+            int? largest = null;
+
             foreach (int n in numbers)
             {
-                if (n > largest)
+                if (largest == null || n > largest)
                 {
                     largest = n;
                 }
             }
-            Console.WriteLine("The largest number is: " + largest);
-        }
+
+            if (largest == null)
+            {
+                Console.WriteLine("No numbers were entered.");
+            }
+            else
+            {
+                Console.WriteLine("The largest number is: " + largest);
+            }
+         }
     }
 
-    [TestClass]
-    public class ProgramTests
-    {
-        [TestMethod]
-        public void ExampleTest()
-        {
-            using FakeConsole console = new FakeConsole("First input", "Second input");
-            Program.Main();
-            Assert.AreEqual("Hello!", console.Output);
-        }
-    }
+    //[TestClass]
+    //public class ProgramTests
+    //{
+    //    [TestMethod]
+    //    public void ExampleTest()
+    //    {
+    //        using FakeConsole console = new FakeConsole("First input", "Second input");
+    //        Program.Main();
+    //        Assert.AreEqual("Hello!", console.Output);
+    //    }
+    //}
 }
